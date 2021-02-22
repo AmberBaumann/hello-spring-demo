3abe6658cededb81617ec9fb9fde7fc660cab358
Amber Baumann <ambermindsolliving@gmail.com>
2021-02-21 18:00:33 -0600

    Started working with thymeleaf

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -8,23 +8,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@ResponseBody
-@RequestMapping("hello")
 public class HelloController {
 
     // Handles requests of the form /hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name){
+    @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public String hello(@RequestParam String name){
         return "Hello, " + name + "!";
     }
 
     // Handles requests of the form /hello/LaunchCode
-    @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name){
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloAgain(@PathVariable String name){
         return "Hello, " + name + "!";
     }
 
-    public String getGreeting(String name, String language){
+    /*public String getGreeting(String name, String language){
         String greeting = "";
         if(language.equals("english")){
             greeting = "Hello";
@@ -42,9 +42,9 @@ public class HelloController {
             greeting = "Hallo";
         }
         return greeting + " " + name;
-    }
+    }*/
 
-    // /hello
+    /*// /hello
     @RequestMapping(value = "hello", method = RequestMethod.GET)
     @ResponseBody
     public String helloGet(@RequestParam String name, @RequestParam String language){
@@ -52,31 +52,33 @@ public class HelloController {
             name = "World";
         }
         return getGreeting(name, language);
-    }
+    }*/
 
-    @RequestMapping(value = "", method = RequestMethod.POST)
+   /* @RequestMapping(value = "", method = RequestMethod.POST)
     @ResponseBody
     public String helloPost(@RequestParam String name, @RequestParam String language){
         return getGreeting(name, language);
-    }
+    }*/
 
-    // /hello/form
+    // /form
     @GetMapping("form")
     public String helloForm(){
-        String html = "<form method = 'get' action= '/hello/hello'>" +
-                "<input type='text' name='name' />" +
-                "\n\n" +
-                "<select name = 'language'>" +
-                "<option value = 'english'>English</option>" +
-                "<option value = 'french'>French</option>" +
-                "<option value = 'spanish'>Spanish</option>" +
-                "<option value = 'italian'>Italian</option>" +
-                "<option value = 'norwegian'>Norwegian</option>" +
-                "</select>" +
-                "\n\n" +
-                "<input type = 'submit' value = 'Greet me!' />" +
-                "</form>";
-        return html;
+        return "form";
     }
 
 }
+
+//String html = "<form method = 'get' action= '/hello/hello'>" +
+//                "<input type='text' name='name' />" +
+//                "\n\n" +
+//                "<select name = 'language'>" +
+//                "<option value = 'english'>English</option>" +
+//                "<option value = 'french'>French</option>" +
+//                "<option value = 'spanish'>Spanish</option>" +
+//                "<option value = 'italian'>Italian</option>" +
+//                "<option value = 'norwegian'>Norwegian</option>" +
+//                "</select>" +
+//                "\n\n" +
+//                "<input type = 'submit' value = 'Greet me!' />" +
+//                "</form>";
+//        return html;
